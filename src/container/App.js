a6c06811a72ee3ea37e4a94f9b15338369618562
mahmoudzanoun <mahmoudzanoun35@yahoo.com>
2022-04-28 01:06:15 +0200

    adding routes and removing test files

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../components/Home/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/program" element={<Program />} /> */}
+          {/* <Route path="/booking" element={<Booking />} /> */}
+          {/* <Route path="/things_to_do" element={<ThingsToDo />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
